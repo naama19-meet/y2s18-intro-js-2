d0beb3f674154735ae3a50c4f28aa69b3fbaf368
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:17:49 +0300

    objects and json

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,15 @@
-// Create your 'me' object literal here!
+var me = {
+
+    firstName: ("naama")
+    lastName: ("shalev")
+    favoriteFoods: ["banana", "ice cream", "pasta"]
+    age : 16,
+    link: "https://www.geckoclimbing.co.il/"
+    picture: "https://r8t6m8f3.stackpathcdn.com/wp-content/uploads/2017/12/climbing-tours-railay-beach-krabi.jpg"
+
+
+
+
+
+
+}// Create your 'me' object literal here!
